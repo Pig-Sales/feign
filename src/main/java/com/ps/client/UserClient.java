0b4d83dedd04012517a699502b45c973745130db
Cbgogogog <1293543528@qq.com>
2023-07-25 22:36:36 +0800

    feat: 增加feign中的判断seller审核接口

--- src/main/java/com/ps/client/UserClient.java
+++ src/main/java/com/ps/client/UserClient.java
@@ -1,5 +1,6 @@
 package com.ps.client;
 
+import com.ps.pojo.Result;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,4 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @FeignClient("user")
 public interface UserClient {
 
+    //true为审核通过，false为其他
+    @PostMapping("/isSellerSafe")
+    Result isSellerSafe(String openId);
 }
