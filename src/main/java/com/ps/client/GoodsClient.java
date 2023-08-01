5b0015106040bce1b5e67df71ee4cb6b8a14315a
wxl <1715142548@qq.com>
2023-08-01 10:14:45 +0800

    fix: feign修改路径

--- src/main/java/com/ps/client/GoodsClient.java
+++ src/main/java/com/ps/client/GoodsClient.java
@@ -5,9 +5,11 @@ import com.ps.pojo.Result;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @FeignClient("goods")
+@RequestMapping("/goods")
 public interface GoodsClient {
 
     @PostMapping("/updateGoodsNumber")
